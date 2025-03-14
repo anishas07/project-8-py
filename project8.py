916026ebe48a162556741796f7096ac684780d42
anishas07 <Studytea95@gmail.com>
2025-03-14 19:52:00 +1030

    Add files via upload

---
+++ project8.py
@@ -0,0 +1,15 @@
+#list 1
+tuple0 = (5, 5, 6, 7, 8, 9, 9)
+print("List 1 of numbers:")
+print(list(tuple0))
+
+#list 2
+tuple1 = ("orange", "mango", "apple", "watermelon")
+print("List 2 of fruits:")
+print(list(tuple1))
+
+#list 3
+tuple2 = ("pink", "green", "blue", "yellow", "black")
+print("List 3 of colours:")
+print(list(tuple2))
+
